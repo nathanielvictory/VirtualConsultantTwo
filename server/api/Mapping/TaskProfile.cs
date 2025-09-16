43c8b3ebe22ee9f2a100ab0be2f33b24fdcb287a
Nathaniel <nathaniel@victorymodeling.com>
2025-09-16 12:21:01 -0400

    Fixing internal server error

--- server/api/Mapping/TaskProfile.cs
+++ server/api/Mapping/TaskProfile.cs
@@ -8,21 +8,28 @@ public class TaskProfile : Profile
 {
     public TaskProfile()
     {
-        // Enums map 1:1 by name
-        CreateMap<TaskJob, TaskListItemDto>();
+        // LIST ITEM: map ctor param "Status" from entity.JobStatus
+        CreateMap<TaskJob, TaskListItemDto>()
+            .ForCtorParam("Status", opt => opt.MapFrom(s => s.JobStatus));
+
+        // DETAIL: also map ctor param "Status"
         CreateMap<TaskJob, TaskDetailDto>()
+            .ForCtorParam("Status", opt => opt.MapFrom(s => s.JobStatus))
             .ForMember(d => d.Artifacts, opt => opt.MapFrom(s => s.Artifacts));
 
         CreateMap<TaskArtifact, TaskArtifactDto>();
 
         CreateMap<CreateTaskDto, TaskJob>()
-            .ForMember(d => d.JobStatus, opt => opt.MapFrom(_ => TaskJobStatus.Queued))
-            .ForMember(d => d.CreatedAt, opt => opt.Ignore())
-            .ForMember(d => d.UpdatedAt, opt => opt.Ignore())
-            .ForMember(d => d.StartedAt, opt => opt.Ignore())
-            .ForMember(d => d.CompletedAt, opt => opt.Ignore());
+            .ForMember(d => d.JobStatus,  opt => opt.MapFrom(_ => TaskJobStatus.Queued))
+            .ForMember(d => d.CreatedAt,  opt => opt.Ignore())
+            .ForMember(d => d.UpdatedAt,  opt => opt.Ignore())
+            .ForMember(d => d.StartedAt,  opt => opt.Ignore())
+            .ForMember(d => d.CompletedAt,opt => opt.Ignore());
 
+        // UPDATE: fix name mismatches so patching Type/Status works
         CreateMap<UpdateTaskDto, TaskJob>()
+            .ForMember(d => d.JobType,   opt => opt.MapFrom(s => s.Type))
+            .ForMember(d => d.JobStatus, opt => opt.MapFrom(s => s.Status))
             .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember is not null));
     }
 }
