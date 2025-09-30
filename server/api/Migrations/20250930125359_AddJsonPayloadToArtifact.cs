e40834a63fcb1b83a02b0ea296f4b21f275afb3c
Nathaniel <nathaniel@victorymodeling.com>
2025-09-30 11:59:41 -0400

    Added migration for payload

---
+++ server/api/Migrations/20250930125359_AddJsonPayloadToArtifact.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJsonPayloadToArtifact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<JsonDocument>(
+                name: "payload",
+                table: "task_artifacts",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "payload",
+                table: "task_artifacts");
+        }
+    }
+}
