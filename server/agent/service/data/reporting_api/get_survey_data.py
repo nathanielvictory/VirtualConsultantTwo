f2b98e235987df3568b4e681a50997a4744531cc
Nathaniel <nathaniel@victorymodeling.com>
2025-10-07 16:33:40 -0400

    Migrating to task based survey json

--- server/agent/service/data/reporting_api/get_survey_data.py
+++ server/agent/service/data/reporting_api/get_survey_data.py
@@ -1,28 +1,40 @@
 import requests
 from pydantic import ValidationError
-import httpx
+import asyncio
 
 from config import settings
 from ..models.survey import Survey
 
 async def get_survey_data(kbid: str, key_number: int = 0) -> Survey | None:
-    reporting_url = f"{settings.REPORTING_URL}/exports/json_exports/survey_data/{kbid}/{key_number}"
+    task_queue_url = f"{settings.REPORTING_URL}/exports/json_exports/survey_data/{kbid}/{key_number}"
+    task_result_url = f"{settings.REPORTING_URL}/exports/json_exports/survey_data/{kbid}/{key_number}/status"
     headers = {
         "X-Username": settings.REPORTING_USERNAME,
         "X-Password": settings.REPORTING_PASSWORD,
     }
+    timeout = 60 * 2
+    total_waited = 0
 
-    timeout = httpx.Timeout(
-        connect=10.0,  # time to establish connection
-        read=240.0,  # time to read response (since it can take ~2 minutes)
-        write=30.0,  # time allowed for sending the request body
-        pool=10.0  # time to wait for a connection from the pool
-    )
-    async with httpx.AsyncClient(timeout=timeout) as client:
-        try:
-            response = await client.get(reporting_url, headers=headers)
+    try:
+        task_response = requests.get(task_queue_url, headers=headers)
+        task_response.raise_for_status()
+        task_id = task_response.json().get('task_id')
+        if not task_id:
+            return None
+        while total_waited < timeout:
+            await asyncio.sleep(2)
+            total_waited += 2
+            response = requests.get(f"{task_result_url}/{task_id}", headers=headers)
             response.raise_for_status()
-            survey_json = response.json()
-            return Survey.model_validate(survey_json)
-        except (httpx.HTTPStatusError, ValidationError, httpx.RequestError) as e:
+            response_json = response.json()
+            if response_json.get('status') in ("pending", "in progress"):
+                continue
+            if response_json.get('status') != 'done':
+                return None
+
+            survey_json = response_json.get("result")
+            survey = Survey.model_validate(survey_json)
+            return survey
+
+    except (requests.exceptions.RequestException, ValidationError) as e:
             return None
